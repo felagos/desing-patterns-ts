1c181e4496e31bbc2adcc5ee675d1ecb0591df69
Felipe Lagos <felagossanchez@gmail.com>
2024-08-12 08:32:09 -0400

    chore: Add builder for forms

---
+++ src/builder/forms-builder.js
@@ -0,0 +1,78 @@
+class Form {
+
+	constructor(controls, action) {
+		this.controls = controls;
+		this.action = action;
+	}
+
+	getContent() {
+		return `
+			<form action="${this.action}" method="POST">
+				${this.controls.map(control => (
+					`<div class="form-item">
+						${this.getLabel(control)}
+						${this.getInput(control)}
+					</div>`
+				))}
+				<button type="submit">Submit</button>
+			</form>
+		`;
+	}
+
+	getLabel(control) {
+		return `<label>${control.text}</label>`;
+	}
+
+	getInput(control) {
+		return `<input type="${control.type}" name="${control.name}" />`;
+	}
+
+}
+
+class FormBuilder {
+
+	constructor() {
+		this.action = '';
+		this.controls = [];
+	}
+
+	setText(name, text) {
+		this.controls.push({
+			name,
+			text,
+			type: 'text',
+		});
+		return this;
+	}
+
+	setCheckbox(name, text) {
+		this.controls.push({
+			name,
+			text,
+			type: 'checkbox',
+		});
+		return this;
+	}
+
+	setAction(action) {
+		this.action = action;
+		return this;
+	}
+
+	build() {
+		return new Form(this.controls, this.action);
+	}
+
+}
+
+const form1 = document.querySelector('.form1');
+
+const formBuilder = new FormBuilder();
+
+const formPeople = formBuilder
+	.setAction('/people')
+	.setText('name', 'Name')
+	.setCheckbox('isStudent', 'Is student')
+	.build();
+
+form1.innerHTML = formPeople.getContent();
